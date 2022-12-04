96e1b1ee10ddd9f3e585cd61762f9d9efb11cac4
Hyeonjun <wns8363@gmail.com>
2022-12-05 00:59:25 +0900

    S1_두수의합
    - 배열

--- B2_ReverseCard/B2_ReverseCard.cpp
+++ B2_ReverseCard/B2_ReverseCard.cpp
@@ -1,5 +1,7 @@
 ﻿// B2_ReverseCard.cpp
 
+
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -17,16 +19,21 @@ void printVector(vector<int> v)
 
 int main()
 {
+    freopen("input.txt", "r", stdin);
     vector<int> v = { 1,2,3,4,5,6,7,8,9,10,
         11,12,13,14,15,16,17,18,19,20 };
 
     printVector(v);
-    for (int i = 0; i < 2; i++)
+    
+
+    for (int i = 0; i < 10; i++)
     {
         int start, end;
         cin >> start >> end;
         reverse(v.begin() + start - 1, v.begin() + end);
     }
+    //reverse(v.begin(), v.end());
+    
     printVector(v);
 
 }
